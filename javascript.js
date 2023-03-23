30ba9574f6a6da7c044b00f726cf0203b00d7107
CameronTheDeveloper <cameronposey@outlook.com>
2023-03-23 04:50:35 -0700

    Book information gets input into HTML

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-let books = [];
-
+const bookList = document.querySelector('#book-list');
+let books = [];             // Stores all Book objects
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
+// Add book to 'books' array
 function addBookToBooks() {
     let title = prompt("Enter title: ");
     let author = prompt("Enter author: ");
@@ -16,10 +17,20 @@ function addBookToBooks() {
 
 function displayBooks() {
     for (i = 0; i < books.length; i++) {
-        //display books[i].name, etc.
+        const newBook = document.createElement('div');
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPages = document.createElement('div');
+        bookList.append(newBook);                   //Add divs to newBook div in HTML
+        newBook.appendChild(bookTitle);
+        newBook.appendChild(bookAuthor);
+        newBook.appendChild(bookPages);
+        bookTitle.innerHTML = books[i].title;       //Add content inside each div
+        bookAuthor.innerHTML = books[i].author;
+        bookPages.innerHTML = books[i].author;
     }
-
 }
 
 addBookToBooks();
+displayBooks();
 
