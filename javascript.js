cd0076d321c3794da24eb573c40a98a27bcafbb6
CameronTheDeveloper <cameronposey@outlook.com>
2023-03-24 08:09:58 -0700

    Form appears on Add Book click, collects input

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-const bookList = document.querySelector('#book-list');      //Book list container
-const newBookButton = document.querySelector('#new-book-button');       //Add Book button
+const bookList = document.querySelector('#book-list');              // Book list container
+const newBookButton = document.querySelector('#new-book-button');   // Add Book button
+const addBookForm = document.querySelector('#add-book-form');       // Form that adds books
 let books = [];   // Stores all Book objects
 let index = 0;    // books index
 function Book(title, author, pages) {
@@ -10,40 +11,48 @@ function Book(title, author, pages) {
 
 // Add book to 'books' array
 function addBookToBooks() {
-    let title = prompt("Enter title: ");
-    let author = prompt("Enter author: ");
-    let pages = prompt("Enter pages: ");
+    let title = addBookForm.getElementById('book-title').value;
+    let author = addBookForm.getElementById('book-author').value;
+    let pages = addBookForm.getElementById('book-pages').value;
     const book = new Book(title, author, pages);
     books.push(book);
     index = displayBooks(index);
 }
 
+// Display the books by looping through 'books' array
 function displayBooks(index) {
     while (index < books.length) {
         const newBook = document.createElement('div');
         const bookTitle = document.createElement('div');
         const bookAuthor = document.createElement('div');
         const bookPages = document.createElement('div');
-        newBook.classList.add("book" /* + index*/);
-        bookTitle.classList.add("book-title");
-        bookAuthor.classList.add("book-author");
-        bookPages.classList.add("book-pages");
-        bookList.append(newBook);                   //Add divs in newBook div in HTML
+        newBook.classList.add('book' /* + index*/);
+        bookTitle.classList.add('title');
+        bookAuthor.classList.add('author');
+        bookPages.classList.add('pages');
+        bookList.append(newBook);
         newBook.appendChild(bookTitle);
         newBook.appendChild(bookAuthor);
         newBook.appendChild(bookPages);
-        bookTitle.innerHTML = "Title: " + books[index].title;       //Add content inside each div
-        bookAuthor.innerHTML = "Author: " + books[index].author;
-        bookPages.innerHTML = "Pages: " + books[index].pages;
+        bookTitle.innerHTML = 'Title: ' + books[index].title;       // Add content inside each div
+        bookAuthor.innerHTML = 'Author: ' + books[index].author;
+        bookPages.innerHTML = 'Pages: ' + books[index].pages;
         index++;
     }
     return index;
 }
 
 newBookButton.addEventListener('click', () => {
-    addBookToBooks();
+    if (addBookForm.style.display == 'none') {
+        addBookForm.style.display = 'block';
+    } else {
+        addBookForm.style.display = 'none';
+    }
 });
 
-
+addBookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToBooks();
+});
 
 
