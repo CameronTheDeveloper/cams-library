fdda7df5676e806a53fe74ff828f6b6f9b230cfa
CameronTheDeveloper <cameronposey@outlook.com>
2023-03-26 00:42:57 -0700

    Javascript.js book-read checkbox input

--- javascript.js
+++ javascript.js
@@ -3,10 +3,11 @@ const newBookButton = document.querySelector('#new-book-button');   // Add Book 
 const addBookForm = document.querySelector('#add-book-form');       // Form that adds books
 let books = [];   // Stores all Book objects
 let index = 0;    // books index
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
 
 // Add book to 'books' array
@@ -14,7 +15,8 @@ function addBookToBooks() {
     let title = document.getElementById('book-title').value;
     let author = document.getElementById('book-author').value;
     let pages = document.getElementById('book-pages').value;
-    const book = new Book(title, author, pages);
+    let read = document.getElementById('book-read').checked;
+    const book = new Book(title, author, pages, read);
     books.push(book);
     index = displayBooks(index);
 }
@@ -26,17 +28,26 @@ function displayBooks(index) {
         const bookTitle = document.createElement('div');
         const bookAuthor = document.createElement('div');
         const bookPages = document.createElement('div');
+        const bookRead = document.createElement('div');
         newBook.classList.add('book' /* + index*/);
         bookTitle.classList.add('title');
         bookAuthor.classList.add('author');
         bookPages.classList.add('pages');
+        bookRead.classList.add('read');
         bookList.append(newBook);
         newBook.appendChild(bookTitle);
         newBook.appendChild(bookAuthor);
         newBook.appendChild(bookPages);
+        newBook.appendChild(bookRead);
         bookTitle.innerHTML = 'Title: ' + books[index].title;       // Add content inside each div
         bookAuthor.innerHTML = 'Author: ' + books[index].author;
         bookPages.innerHTML = 'Pages: ' + books[index].pages;
+        if (books[index].read) {    //Book read
+            bookRead.innerHTML = 'Read';
+        } else {
+            bookRead.innerHTML = 'Unread';
+        }
+
         index++;
     }
     return index;
